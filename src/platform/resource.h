393600a891ef08a9854eefba44a2e1eaaec195d9
CW-KeithP <kp+github@auth8.net>
2025-06-18 13:43:07 +0800

    Added icon, split rc file up, improved rc versioning etc

---
+++ src/platform/resource.h
@@ -0,0 +1,38 @@
+/*
+ * npad - Resource Header
+ * Resource ID definitions for Windows resources
+ *
+ * Author: Platima
+ * https://github.com/platima/npad
+ */
+
+#ifndef RESOURCE_H
+#define RESOURCE_H
+
+// Application Icon
+#define IDI_NPAD                        100
+
+// Dialog IDs
+#define IDD_FIND                        200
+#define IDD_REPLACE                     201
+
+// Control IDs for Find Dialog
+#define ID_FIND_TEXT                    3001
+#define ID_FIND_NEXT                    3002
+#define ID_FIND_CANCEL                  3003
+#define ID_FIND_CASE                    3004
+#define ID_FIND_WHOLE_WORD              3005
+
+// Control IDs for Replace Dialog
+#define ID_REPLACE_WITH                 3006
+#define ID_REPLACE_NEXT                 3008
+#define ID_REPLACE_ALL                  3009
+
+// Direction radio buttons
+#define IDC_RADIO_UP                    3010
+#define IDC_RADIO_DOWN                  3011
+
+// Static text controls
+#define IDC_STATIC                      -1
+
+#endif // RESOURCE_H
